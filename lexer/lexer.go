45218f7fc0373d2fb15149c363d6e7bc6f4d0979
Will Weaver <will_weaver15@hotmail.com>
2025-09-21 20:13:56 -0500

    1.3 Lexer. Add lexer and mod file

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,40 +1,62 @@
-//lexer/lexer_test.go
+//lexer/lexer.go
 
 package lexer
 
-import{
-	"testing"
-	"Interpreter/token"
+import "Interpreter/token"
+
+type Lexer struct {
+	input        string
+	position     int  //current poistion in input (points to current char)
+	readPosition int  //current reading position in input (after current char)
+	ch           byte //current char under examination
 }
 
-func TestNextToke(t *testing.T){
-	input := '=+(){},;'
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
 
-	tests := []struct {
-		expectedType token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN , "("},
-		{token.RPAREN , ")"},
-		{token.LBRACE , "{"},
-		{token.RBRACE , "}"},
-		{token.COMMA , ","},
-		{token.SEMICOLON , ";"},
-		{token.EOF , ""}
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 //reached end, ASCII for NUL
+	} else {
+		l.ch = l.input[l.readPosition]
 	}
 
-	l := New(input)
-	for i, tt := range test{
-		tok := l.NextToken()
+	l.position = l.readPosition
+	l.readPosition += 1
+}
 
-		if tok.Type != tt.expectedType{
-			t.Fatalf("test[%d] - tokentype wrong. expcted=%q, got=%q", i, tt.expectedType, tok.Type)
-		}
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
 
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("test[%d] - literal wrong. expected=%q, got=%q", i. tt.expectedLiteral, tok.Literal)
-		}
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
 	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os/user"
+)
 
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! Feel free to type in commands\n", user.Username)
+	//repl.Start(os.Stdin, os.Stdout)
 }
