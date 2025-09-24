df1bba063a3821f93ad8778421fbfa9b1a08b5f5
Will Weaver <will_weaver15@hotmail.com>
2025-09-23 19:39:15 -0500

    1.3 Lexer. Expand lexer to read letters, numbers and skip white space

--- token/token.go
+++ token/token.go
@@ -33,3 +33,17 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	//check to see if identifier is in keyword table
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	//token type for user-defined identifiers
+	return IDENT
+}
